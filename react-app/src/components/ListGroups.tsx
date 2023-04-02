00a95ba24e4f80821228ed35ceacd05ddbb98453
ItzNotAsh <chawlaashneet@gmail.com>
2023-04-02 22:04:59 +0530

    event handeling

--- react-app/src/components/ListGroups.tsx
+++ react-app/src/components/ListGroups.tsx
@@ -1,20 +1,24 @@
+import { MouseEvent } from "react";
+
+const clickHandler = (event: MouseEvent) => {
+  console.log(event);
+};
+
 function ListGroup() {
-    let items = ["paris", "london", "canada", "autralia", "punjab", "usa"];
-    // items = [];
-    return (
-        <>
-            <h1>Hello world</h1>
-            {/* {items.length === 0 ? <p>No items</p> : null} */}
-            {items.length === 0 && <p>No items</p>}
-            <ul className="list-group">
-                {items.map((item) => (
-                    <li className="list-group-item" key={item}>
-                        {item}
-                    </li>
-                ))}
-            </ul>
-        </>
-    );
+  let items = ["paris", "london", "canada", "autralia", "punjab", "usa"];
+  return (
+    <>
+      <h1>Hello world</h1>
+      {items.length === 0 && <p>No items</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" onClick={clickHandler} key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;

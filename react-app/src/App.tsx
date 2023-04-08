ce0fd94ab92eaa892e77904f5533337849ee3ea3
ItzNotAsh <chawlaashneet@gmail.com>
2023-04-08 22:59:20 +0530

    bug fix

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,22 +1,21 @@
 import ListGroup from "./components/ListGroups";
+import ProjectTemplate from "./utils/ProjectTemplate";
 import MouseFollower from "./components/MouseFollower";
 import Navbar from "./components/Navbar";
-
 function App() {
-  let items = ["paris", "london", "canada", "autralia", "punjab", "usa"];
-
-  const selectHandle = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <>
       <div>
         <div className="container">
           <div className="containerBox">
             <MouseFollower />
-            {/* <Navbar /> */}
+            <Navbar />
           </div>
+        </div>
+      </div>
+      <div className="container-2">
+        <div className="test">
+          <ProjectTemplate />
         </div>
       </div>
     </>
